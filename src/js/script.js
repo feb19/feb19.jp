9d13f5f894df6c2678b5c29dc7b0f90dc3408598
Nobuhiro Takahashi <nt@feb19.jp>
2016-03-15 01:45:44 +0900

    add fullscreen button

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+(function() {
+function clickHandler(event) {
+  var elem = document.body;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  }
+};
+document.addEventListener('DOMContentLoaded', function() {
+  var btn = document.createElement('div');
+  btn.className = 'btn-fullscreen';
+  btn.innerHTML = '<i class="fa fa-expand"></i>';
+  btn.addEventListener('click', clickHandler, false);
+  var elem = document.body;
+  if (elem.requestFullscreen) {
+    elem.appendChild(btn);
+  } else if (elem.msRequestFullscreen) {
+    elem.appendChild(btn);
+  } else if (elem.mozRequestFullScreen) {
+    elem.appendChild(btn);
+  } else if (elem.webkitRequestFullscreen) {
+    elem.appendChild(btn);
+  }
+}, false);
+})();
